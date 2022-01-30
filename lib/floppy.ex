ed0a48c4712baa8e745c7c1be9f41178f3725935
Dmitry Shpagin <dmitry.shpagin@balance-pl.ru>
2022-01-30 20:52:10 +0300

    json assert

--- lib/floppy.ex
+++ lib/floppy.ex
@@ -3,6 +3,38 @@ defmodule Floppy do
   Documentation for `Floppy`.
   """
 
+  defmacro json_assert(result, options \\ []) do
+    name = get_name(binding())
+
+    quote do
+      env = __ENV__
+
+      dir = String.replace(env.file, ".exs", "")
+
+      {file, _} = env.function
+      name = unquote(name)
+
+      path =
+        (dir <> "/" <> to_string(file) <> inspect(unquote(name)) <> ".json")
+        |> String.replace(["\""], "")
+        |> String.replace([" "], "_")
+
+      result = Jason.encode!(unquote(result), pretty: true)
+
+      if !File.exists?(path) || System.get_env("FLOPPY_MODE") == "rewrite" do
+        File.mkdir_p!(dir)
+
+        File.write!(path, result)
+
+        assert true
+      else
+        previous_result = File.read!(path)
+
+        assert Jason.decode!(previous_result) == Jason.decode!(result)
+      end
+    end
+  end
+
   defmacro assert(result, options \\ []) do
     name = get_name(binding())
 
---
+++ test/floppy_json_test.exs
@@ -0,0 +1,23 @@
+defmodule FloppyJsonTest do
+  use ExUnit.Case
+  require Floppy
+
+  test "map saves with pretty format" do
+    map =
+      0..1024
+      |> Enum.to_list()
+      |> Enum.map(fn i -> {i, i * 2} end)
+      |> Map.new()
+
+    Floppy.json_assert(map)
+  end
+
+  test "list check" do
+    list_1024 =
+      0..1024
+      |> Enum.to_list()
+
+    Floppy.json_assert(list_1024)
+    Floppy.json_assert([])
+  end
+end
